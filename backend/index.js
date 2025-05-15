e3dfd517016056035c86037a3790111a61ab8b9a
Divijjain25 <divijjain25@gmail.com>
2025-05-15 18:00:38 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
@@ -32,6 +33,13 @@ app.get('/', (req, res) => {
   res.send('Backend server is running!');
 });
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'frontend/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+  });
+}
 app.listen(port, () => {
   console.log(`App is running on ${port}`);
 });
